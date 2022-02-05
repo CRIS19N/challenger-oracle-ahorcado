d0c5afae13951dc9f4f70a3ad78a3d2b7e98119d
Cristian D. Cardona V <cris11cristian@gmail.com>
2022-02-05 13:09:38 -0500

    Agregando archivos json lista-palabras

---
+++ src/js/lista_palabras.js
@@ -0,0 +1,42 @@
+[
+  {
+    words: {
+      word: [
+        "Alura",
+        "Abeja",
+        "Abejorro",
+        "Alce",
+        "Anguila",
+        "Armadillo",
+        "Asno",
+        "Avestruz",
+        "Avispa",
+
+        "Babosa",
+        "Ballena",
+        "Buey",
+        "Buitre",
+        "Burro",
+
+        "Caballo",
+        "Camello",
+        "Canguro",
+        "Canario",
+        "Cangrejo",
+        "Caracol",
+        "Castor",
+        "Cebra",
+        "Cerdo",
+        "Ciervo",
+        "Cisne",
+        "Cocodrilo",
+        "Comadreja",
+        "Conejo",
+        "Coral",
+        "Coyote",
+
+        "Oracle",
+      ],
+    },
+  },
+];
