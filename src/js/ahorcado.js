3ca373d84da26126ba9f7fb07fe76b07a7f60242
Cristian D. Cardona V <cris11cristian@gmail.com>
2022-02-06 11:56:29 -0500

    Finalizada las funciones del ahorcado

--- src/js/ahorcado.js
+++ src/js/ahorcado.js
@@ -2,6 +2,12 @@
 let canvasWidth = CANVAS.width;
 let canvasHeight = CANVAS.height;
 let grosoLinea = 15;
+let puedeJugar = false;
+let posicionLetras = []; //se almacena cada letra de la palabra oculta y su posicion
+let letrasPresionadas = []; //se almacena las letras presionadas en el juego
+let palabraAleatoria = []; //se almacena la palabra completa oculta
+let letrasAcertadas = "";
+let cantidadIntentos = 0;
 
 function lineaVertical() {
   linea(180, 60, 180, 690, "red", grosoLinea);
@@ -14,7 +20,6 @@ function lineaHorizontal() {
 function lazo() {
   linea(580, 60, 580, 200, "brown", grosoLinea);
 }
-
 function cabeza() {
   circulo();
 }
@@ -47,23 +52,135 @@ function newWord() {
   newWord.value = word;
   if (word.length > 16 || !result) {
     if (word.length > 16) {
+      pintarMensajeFinal("La palabra no puede ser mayor a 16 dígitos", 400, 47);
+
       console.warn(`La palabra no puede ser mayor a 16 dígitos`);
     } else if (!result) {
+      pintarMensajeFinal(
+        "No puede tener caracteres extraños ó números",
+        400,
+        47
+      );
       console.warn(`No puede tener caracteres extraños ó números`);
     }
+
     newWord.value = "";
   } else {
     if (!JSONWORDS.includes(word)) {
       JSONWORDS.push(word);
     }
     newWord.value = "";
-    alert("Se agrego correctamente la palabra");
+    pintarMensajeFinal("Se agrego correctamente la palabra", 400, 47);
+  }
+
+  setTimeout(() => {
+    console.log("me ejecute");
+    pintarMensajeFinal(" ", 500, 360);
+  }, 300);
+}
+
+function seleccionarPalabra() {
+  let rand = Math.floor(Math.random() * JSONWORDS.length);
+  let palabra = JSONWORDS[rand].toUpperCase();
+  return palabra;
+}
+
+function pintarEspacios(palabra) {
+  //si encuentra espacios en la palabra escogida lo pinta en pantalla
+  palabra.forEach((letra) => {
+    if (letra[0] == " ") {
+      lineaText("-", letra[1] + 17, 755);
+    }
+  });
+}
+
+function pintarLineaLetra(palabra, letraXDesde, letraYHasta, positionY) {
+  console.log(palabra);
+  posicionLetras = [];
+  for (let i = 0; i < palabra.length; i++) {
+    const letra = palabra[i];
+
+    linea(
+      letraXDesde,
+      positionY,
+      letraYHasta,
+      positionY,
+      "black",
+      grosoLinea - 10
+    ); //LINEA TEXT
+
+    posicionLetras.push([letra, letraXDesde]);
+    /*  lineaText(letra, letraXDesde + 7 + 10, 755);*/
+    letraXDesde += 50;
+    letraYHasta += 50;
+  }
+}
+let posicionLetraPresionada = 0;
+function pintarLetrasPresionadas(letraPresionada) {
+  ///pintar letras presionadas
+  lineaText(letraPresionada, posicionLetraPresionada + 10, 120);
+
+  posicionLetraPresionada += 25;
+}
+
+function PintarLetraEnLinea(palabra, letraSearch) {
+  palabra.forEach((letra) => {
+    if (letra[0] == letraSearch) {
+      letrasAcertadas += letra[0];
+      lineaText(letra[0], letra[1] + 10, 755);
+    }
+  });
+}
+
+function pintarMensajeFinal(palabra, positionX, positionY) {
+  lineaText(palabra, positionX, positionY);
+}
+
+function comprobarPalabraOculta() {
+  if (palabraAleatoria.length > 0) {
+    if (
+      letrasAcertadas.length == palabraAleatoria.length &&
+      cantidadIntentos < 9
+    ) {
+      return "gano";
+    } else if (cantidadIntentos == 9) {
+      return "perdio";
+    } else {
+      return "Estas en Juego";
+    }
+  } else {
+    puedeJugar = false;
   }
 }
 
-function jugar(params) {
-  var rand = ~~(Math.random() * myArray.length);
-  console.log(JSONWORDS.length);
+function jugar() {
+  clearPizarra(); //limpiar canvas
+  iniciarPizarra(); //iniciar pizarra
+  puedeJugar = true;
+  letrasPresionadas = [];
+  letrasAcertadas = "";
+  cantidadIntentos = 0;
+  posicionLetraPresionada = 640;
+
+  //desde donde se pintara la primera palabra
+  //debe iniciar desde x 350 y=760 hasta x=750 y=760 esto le da un tamaño de 59px
+  let letraXDesde = 340;
+  let letraYHasta = 380;
+  let positionY = 760;
+  let widthLinea = grosoLinea - 10;
+
+  palabraAleatoria = seleccionarPalabra();
+
+  pintarEspacios(posicionLetras);
+  pintarLineaLetra(
+    palabraAleatoria,
+    letraXDesde,
+    letraYHasta,
+    positionY,
+    widthLinea
+  );
+
+  //escuchar evento del teclado
 }
 //agregar nueva palabra
 const BTNWORDNEW = document.getElementById("nueva-palabra");
@@ -71,57 +188,66 @@ BTNWORDNEW.onclick = newWord;
 
 //btn iniciar Juego
 const BTNPLAY = document.getElementById("iniciar-juego");
+BTNPLAY.onclick = jugar;
 
-/* linea(350, 760, 400, 760, "black", grosoLinea - 10); //pie derecha */
+document.addEventListener("keyup", function (e) {
+  if (puedeJugar) {
+    let letraPresionada = e.key.toLocaleUpperCase();
 
-//desde donde se pintara la primera palabra
-//debe iniciar desde x 350 y=760 hasta x=750 y=760 esto le da un tamaño de 59px
-let letraXDesde = 340;
-let letraYHasta = 380;
-let positionY = 760;
-let widthLinea = grosoLinea - 10;
+    //agregamos la letra presionada al array
+    if (!letrasPresionadas.includes(letraPresionada)) {
+      //también comprobamos si esa letra esta en la palabra oculta, sino pintamos parte del ahorcado
+      if (!palabraAleatoria.includes(letraPresionada)) {
+        cantidadIntentos += 1;
+        switch (cantidadIntentos) {
+          case 1:
+            lineaVertical();
+            break;
+          case 2:
+            lineaHorizontal();
+            break;
+          case 3:
+            lazo();
+            break;
+          case 4:
+            cabeza();
+            break;
+          case 5:
+            tronco();
+            break;
+          case 6:
+            manoIzquierda();
+            break;
+          case 7:
+            manoDerecha();
+            break;
+          case 8:
+            pieDerecho();
+            break;
+          case 9:
+            pieIzquierdo();
+            break;
+        }
+      }
+      letrasPresionadas.push(letraPresionada); //push de letrasPresionadas
+      pintarLetrasPresionadas(letraPresionada); //Pintar letras presionadas en canvas
+    }
 
-let letras = "NAME IS CRISTIAN";
-if (letras.length > 16) {
-  console.log(
-    "letras " +
-      letras.length +
-      " excedió el tamaño - el temaño permitido es 16 letras"
-  );
-}
-let posicionLetras = [];
-
-for (let i = 0; i < letras.length; i++) {
-  const letra = letras[i];
-  linea(
-    letraXDesde,
-    positionY,
-    letraYHasta,
-    positionY,
-    "black",
-    grosoLinea - 10
-  ); //LINEA TEXT
-
-  posicionLetras.push([letra, letraXDesde]);
-  /*  lineaText(letra, letraXDesde + 7 + 10, 755);*/
-  letraXDesde += 50;
-  letraYHasta += 50;
-}
-
-for (let index = 0; index < posicionLetras.length; index++) {
-  const letra = posicionLetras[index];
-  if (letra[0] == "I") {
-    lineaText(letra[0], letra[1] + 17, 755);
-
-    //letra[0];
+    if (!letrasAcertadas.includes(letraPresionada)) {
+      //pintamos letra en la linea si es encontrada
+      PintarLetraEnLinea(posicionLetras, letraPresionada);
+    }
   }
 
-  if (letra[0] == " ") {
-    lineaText("-", letra[1] + 17, 755);
+  //comprobando si el usuario puede jugar
 
-    //letra[0];
+  let result = comprobarPalabraOculta();
+
+  if (result == "perdio") {
+    pintarMensajeFinal("Fin del juego!", 800, 320);
+    puedeJugar = false;
+  } else if (result == "gano") {
+    pintarMensajeFinal("Ganaste \n Felicidades!", 800, 320);
+    puedeJugar = false;
   }
-}
-/* 
-clearPizarra(); //limpiarcanvas
-iniciarPizarra(); //iniciar pizara */
+});
